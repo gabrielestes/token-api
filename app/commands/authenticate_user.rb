0b1de5884fcdca05c76a0a64ff2dc42b02832ae2
Gabriel Estes <gabriel.estes@gmail.com>
2016-11-05 21:02:52 -0400

    Add class AuthenticateUser

---
+++ app/commands/authenticate_user.rb
@@ -0,0 +1,26 @@
+# Checks a user's email and password, returns user if there's a match
+
+class AuthenticateUser
+  prepend SimpleCommand
+
+  def initialize()
+    @email = email
+    @password = password
+  end
+
+  def call
+    JsonWebToken.encode(user_id: user.id) if user
+  end
+
+  private
+
+  attr_accessor :email, :password
+
+  def user
+    user = User.find_by_email(email)
+    return user if user && user.authenticate(password)
+
+    errors.add :user_authentication, 'invalid credentials'
+    nil
+  end
+end
